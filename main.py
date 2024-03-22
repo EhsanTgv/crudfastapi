8606e5d4ea728afe04a860a919f5b3d0ac428f13
EhsanTgv <EhsanTgv@gmail.com>
2024-03-22 09:25:59 +0300

    Implement Post request

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import schemas
 
 app = FastAPI()
 
@@ -15,3 +16,24 @@ def getItems():
 @app.get("/{id}")
 def getItem(id:int):
     return fakeDatabase[id]
+
+# #option #1
+# @app.post("/")
+# def addItem(task:str):
+#     newId = len(fakeDatabase.keys())+1
+#     fakeDatabase[newId] = {"task":task}
+#     return fakeDatabase
+
+# #option #2
+# @app.post("/")
+# def addItem(item:schemas.Item):
+#     newId = len(fakeDatabase.keys())+1
+#     fakeDatabase[newId] = {"task":item.task}
+#     return fakeDatabase
+
+#option #3
+@app.post("/")
+def addItem(body = Body()):
+    newId = len(fakeDatabase.keys())+1
+    fakeDatabase[newId] = {"task":body['task']}
+    return fakeDatabase
