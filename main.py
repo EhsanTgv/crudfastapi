f74e3a3f49d46c6913faa2f4271f517df16eef48
EhsanTgv <EhsanTgv@gmail.com>
2024-04-26 09:09:37 +0300

    Implement Put request

--- main.py
+++ main.py
@@ -25,15 +25,20 @@ def getItem(id:int):
 #     return fakeDatabase
 
 # #option #2
-# @app.post("/")
-# def addItem(item:schemas.Item):
-#     newId = len(fakeDatabase.keys())+1
-#     fakeDatabase[newId] = {"task":item.task}
-#     return fakeDatabase
+@app.post("/")
+def addItem(item:schemas.Item):
+    newId = len(fakeDatabase.keys())+1
+    fakeDatabase[newId] = {"task":item.task}
+    return fakeDatabase
 
 #option #3
-@app.post("/")
-def addItem(body = Body()):
-    newId = len(fakeDatabase.keys())+1
-    fakeDatabase[newId] = {"task":body['task']}
+# @app.post("/")
+# def addItem(body = Body()):
+#     newId = len(fakeDatabase.keys())+1
+#     fakeDatabase[newId] = {"task":body['task']}
+#     return fakeDatabase
+
+@app.put("/{id}")
+def updateItem(id:int,item:schemas.Item):
+    fakeDatabase[id]['task'] = item.task
     return fakeDatabase
